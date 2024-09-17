4f2ea4d30cf790bfde3d5359e6fde12aa520e11b
Paulo Henrique PErei <74430831+paulofreyyy@users.noreply.github.com>
2024-09-17 14:45:24 -0300

    feat: svg color change

--- scripts/script.js
+++ scripts/script.js
@@ -14,7 +14,7 @@ function fetchGames(round) {
             // Encontra a rodada correspondente
             const roundData = data.find(r => r.round === round);
 
-            // Encontra o numero maximo de rodadas
+            // Encontra o número máximo de rodadas
             maxRounds = data.length;
 
             // Se existir a rodada, exibe os jogos
@@ -29,7 +29,14 @@ function fetchGames(round) {
                     gameDiv.innerHTML = `
                         <div class="gameLine">
                             <div class='teamInfos'>
-                                <img src='./assets/team_shield.svg' class='svg-icon'/>
+                                <svg width="32" height="40" viewBox="0 0 32 40" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill="url(#${game.team_home_id})" d="M16 0C14.8835 1.49628 14.1602 4.31649 9.54496 5.01371C9.11253 5.07638 8.70368 5.10772 8.31056 5.10772C5.38575 5.10772 3.61671 3.51743 3.61671 3.51743L0 7.31688C0 7.31688 5.59803 9.10302 1.11646 25.1156C-1.91843 35.9577 14.5926 37.6028 16 40C17.3995 37.6028 33.9106 35.9577 30.8835 25.1156C26.4098 9.10302 32 7.31688 32 7.31688L28.3754 3.51743C28.3754 3.51743 26.6064 5.10772 23.6816 5.10772C23.2885 5.10772 22.8796 5.07638 22.4472 5.01371C17.8398 4.32432 17.1165 1.49628 15.9921 0L16 0Z"/>
+                                    <defs>
+                                    <linearGradient id="${game.team_home_id}" x1="16" y1="0" x2="16" y2="40" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="var(--color-stop)"/>
+                                        <stop offset="1" stop-color="var(--color-stop)" stop-opacity="0.3"/>
+                                    </linearGradient>
+                                </svg>
                                 <p>${game.team_home_name}</p>
                             </div>
 
@@ -41,7 +48,14 @@ function fetchGames(round) {
 
                             <div class='teamInfos'>
                                 <p>${game.team_away_name}</p>
-                                <img src='./assets/team_shield.svg' />
+                                <svg width="32" height="40" viewBox="0 0 32 40" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill="url(#${game.team_away_id})" d="M16 0C14.8835 1.49628 14.1602 4.31649 9.54496 5.01371C9.11253 5.07638 8.70368 5.10772 8.31056 5.10772C5.38575 5.10772 3.61671 3.51743 3.61671 3.51743L0 7.31688C0 7.31688 5.59803 9.10302 1.11646 25.1156C-1.91843 35.9577 14.5926 37.6028 16 40C17.3995 37.6028 33.9106 35.9577 30.8835 25.1156C26.4098 9.10302 32 7.31688 32 7.31688L28.3754 3.51743C28.3754 3.51743 26.6064 5.10772 23.6816 5.10772C23.2885 5.10772 22.8796 5.07638 22.4472 5.01371C17.8398 4.32432 17.1165 1.49628 15.9921 0L16 0Z"/>
+                                    <defs>
+                                    <linearGradient id="${game.team_away_id}" x1="16" y1="0" x2="16" y2="40" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="var(--color-stop)"/>
+                                        <stop offset="1" stop-color="var(--color-stop)" stop-opacity="0.3"/>
+                                    </linearGradient>
+                                </svg>
                             </div>
                         </div>
                     `;
